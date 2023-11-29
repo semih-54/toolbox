021bb4f4d03eecd04b5c9911d3e187760e796b63
Pierre Cahuzac <pierre.cahuzac1@gmail.com>
2023-11-29 18:14:43 +0800

    wed

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,2 +1,32 @@
 class AppsController < ApplicationController
+  def index
+    @apps = App.all
+  end
+
+  def show
+    @app = App.find(params[:id])
+  end
+
+  def new
+    @app = App.new
+  end
+
+  def create
+    @app = App.new(app_params)
+    if @app.save
+      redirect_to apps_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @app = App.find(params[:id])
+  end
+
+  private
+
+  def app_params
+    params.require(:app).permit(:name, :description, :votes)
+  end
 end
