c7c1e66368bd1e75278902d4e9a27f3189b2526b
Nicholas Matthew <nicholasmatthew200@gmail.com>
2023-12-01 11:20:21 +0800

    deleted unused js

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,7 +1,5 @@
-
-
 // app/javascript/controllers/filter_controller.js
-import { Controller } from "stimulus";
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   submit() {
