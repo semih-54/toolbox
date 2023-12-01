6e98521bcedfd9cbc4ddf85b0a5ad12f39dd2557
Semih cakmak <cakmak.semih@hotmail.com>
2023-12-01 12:01:24 +0800

    Merged both user and connection versions

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,17 @@ class User < ApplicationRecord
   def send_link(user)
     connections.create(receiver_id: user.id)
   end
+
+  def connections
+    Connection
+      .where("(asker_id = ? OR receiver_id = ?) AND status='accepted'", id, id)
+      .map { |connection| connection.asker == self ? connection.receiver : connection.asker }
+  end
+
+  def connected_with?(user)
+    asked = connections.where("(asker_id = ? AND receiver_id = ?) AND status='accepted'", id, user.id).any?
+    received = connections.where("(asker_id = ? AND receiver_id = ?) AND status='accepted'", user.id, id).any?
+
+    asked || received
+  end
 end
