d59d76dfbe3e89b1c75b81af356dbaafccebf4eb
Pierre Cahuzac <pierre.cahuzac1@gmail.com>
2023-12-07 20:25:57 +0800

    final

--- app/javascript/controllers/votes_controller.js
+++ app/javascript/controllers/votes_controller.js
@@ -2,7 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="votes"
 export default class extends Controller {
+  static targets = [ "app" ]
+
   connect() {
-    console.log("Hello, votes!", this.element)
+    
+  }
+
+  vote(event) {
+    event.preventDefault()
+    fetch(event.target.action, {
+      method: event.target._method ? event.target._method.value.toUpperCase() : event.target.method,
+      headers: {
+        "Accept": "text/plain",
+        "Content-Type": "multipart/form-data",
+        "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content
+      },
+      body: new FormData(event.target)
+    })
+    .then(response => response.text())
+    .then(html => {
+      console.log("success", html)
+      this.element.innerHTML = html
+    }
+    )
   }
 }
